5234bd2e8fc85a48eb3f53f2f94e1a7f09cfd5ae
Rajashree <rajashree0507@gmail.com>
2026-04-23 18:09:09 +0530

    added details

--- day3_duplicate_array.py
+++ day3_duplicate_array.py
@@ -1,3 +1,9 @@
+# Problem: Contains Duplicate
+# Platform: LeetCode
+# Difficulty: Easy
+# Approach: Set
+# Time: O(n)
+# Space: O(n)
 def containsDuplicate(nums):
     return len(nums) != len(set(nums))
 nums = [1,2,3,1]
