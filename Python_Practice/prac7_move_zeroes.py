7d635b7e6b2be469c9a272e2acdb9a331177e866
Rajashree <rajashree0507@gmail.com>
2026-05-19 21:58:22 +0530

    Added Move Zeroes problem

---
+++ Python_Practice/prac7_move_zeroes.py
@@ -0,0 +1,17 @@
+# Problem: Move Zeroes
+# Platform: LeetCode
+# Difficulty: Easy
+# Approach: Two Pointers
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def moveZeroes(nums):
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+    return nums
+
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
